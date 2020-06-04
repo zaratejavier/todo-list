945229e145e1396810226532d8de1d0c832a6750
Javier Zarate <zarate.javier.jz@gmail.com>
2020-06-03 20:17:35 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+axios
+  .get("https://jsonplaceholder.typicode.com/todos")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
